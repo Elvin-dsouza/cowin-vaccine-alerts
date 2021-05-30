0affe36d05f9a60ac7ea3251b4e887b96e82a578
Elvin-dsouza <sdsouza3011@gmail.com>
2021-05-30 15:12:48 +0530

    Update: fix bottom overflow

--- lib/main.dart
+++ lib/main.dart
@@ -89,6 +89,14 @@ class _InputFiltersState extends State<InputFilters> {
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
+                  TextFormField(
+                      controller: filterPinController,
+                      decoration: InputDecoration(
+                          hintText: "Enter Pin Code", labelText: "Pin Code")),
+                  TextFormField(
+                      controller: filterAgeController,
+                      decoration: InputDecoration(
+                          hintText: "Enter Age Restriction", labelText: "Age")),
                   DropdownButtonFormField(
                       value: filterDose,
                       decoration: InputDecoration(labelText: "Dose#"),
@@ -105,14 +113,6 @@ class _InputFiltersState extends State<InputFilters> {
                           filterDose = value;
                         });
                       }),
-                  TextFormField(
-                      controller: filterPinController,
-                      decoration: InputDecoration(
-                          hintText: "Pin Code", labelText: "Pin Code")),
-                  TextFormField(
-                      controller: filterAgeController,
-                      decoration: InputDecoration(
-                          hintText: "Age Group", labelText: "Age")),
                   Container(
                     child: Row(
                       children: [
@@ -177,7 +177,7 @@ class _InputFiltersState extends State<InputFilters> {
                           child:
                               lastUpdatedOn != null && lastUpdatedOn.length > 0
                                   ? Text("$lastUpdatedOn")
-                                  : null)
+                                  : Text("No Information Available"))
                     ],
                   ),
                   // ElevatedButton(
